9a005d62c20b2d63c9301440bba0852c1d18cd7c
isaev <isaev@kaluga.biztrend.ru>
2019-05-22 16:40:54 +0300

    Add API_URL

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ const usersRouter = require('./routes/sms');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 // Обработка cors запросов, если клиент на другом домене
 app.use(require('cors')());
@@ -21,8 +17,25 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/api/sms', usersRouter);
+
+//================Развертывание приложения в production================
+if (process.env.NODE_ENV === 'production') {
+  console.log('-----Сервер app запущен в production-----');
+  const pathFolder = path.resolve('client', 'build');
+  app.use(express.static(pathFolder));
+  app.get('*', (req, res) => {
+    const pathIndex = path.resolve(pathFolder, 'index.html');
+    res.sendFile(pathIndex)
+  });
+} else {
+  app.use("/favicon.ico", express.static('favicon.ico'));
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+  app.use('/', indexRouter);
+}
+//================
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- client/src/ApiUrl.js
+++ client/src/ApiUrl.js
@@ -1,4 +1,4 @@
 // Страница авторизации для продакшина
 export const ApiUrl = {
-  base: "http://localhost:5500"
+  base: process.env.API_URL
 };
