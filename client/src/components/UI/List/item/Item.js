65f1ce6980c4d053e1dee00e4c1b938b2b5cea97
isaev <isaev@kaluga.biztrend.ru>
2019-07-31 14:06:40 +0300

    Release preparation, fix

--- client/src/components/UI/List/item/Item.js
+++ client/src/components/UI/List/item/Item.js
@@ -12,8 +12,8 @@ const Item = props => {
     <li className={cls.join(' ')}>
       {props.answer.name} - {props.answer.text}
       <small>
-        {props.answer.isMobile ? <i className="fas fa-sync-alt">&nbsp;</i> : null}
-        {props.answer.isAutoMobile ? <i className="fas fa-mobile-alt" aria-hidden="true">&nbsp;</i> : null}
+        {props.answer.isAutoMobile ? <i className="fas fa-sync-alt">&nbsp;</i> : null}
+        {props.answer.isMobile ? <i className="fas fa-mobile-alt">&nbsp;</i> : null}
 
         {date.toLocaleDateString()} {date.toLocaleTimeString()}
       </small>
